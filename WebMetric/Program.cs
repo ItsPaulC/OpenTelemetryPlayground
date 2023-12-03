67b6e3ee07ef6d51542fbce44d9a9d6c6aaad11e
Paul C <paulcoffey405@gmail.com>
2023-12-03 14:12:24 -0600

    working metrics

---
+++ WebMetric/Program.cs
@@ -0,0 +1,34 @@
+using OpenTelemetry.Metrics;
+
+//C:\dev\repos\OpenTelemetryPlayground\WebMetric.csproj
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOpenTelemetry()
+    .WithMetrics(builder =>
+    {
+        builder.AddPrometheusExporter();
+
+        builder.AddMeter("Microsoft.AspNetCore.Hosting",
+            "Microsoft.AspNetCore.Server.Kestrel");
+        builder.AddView("http.server.request.duration",
+            new ExplicitBucketHistogramConfiguration
+            {
+                Boundaries = new double[]
+                {
+                    0, 0.005, 0.01, 0.025, 0.05,
+                    0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10
+                }
+            });
+    });
+var app = builder.Build();
+
+app.MapPrometheusScrapingEndpoint();
+
+app.MapGet("/", () => HelloOpentelemetryTicks());
+
+string HelloOpentelemetryTicks()
+{
+    return $"Hello OpenTelemetry! ticks: {DateTime.Now.Ticks.ToString()[^3..]}";
+}
+
+
+app.Run();
